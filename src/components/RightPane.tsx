d93c0fdf74a8ce0dfc0f0d592a2f865df39526eb
Arun Saragadam <iarunsaragadam@gmail.com>
2025-07-01 18:44:02 +0100

    feat: allow code editor and test results to be resized to zero, default to 50/50 split

--- src/components/RightPane.tsx
+++ src/components/RightPane.tsx
@@ -21,6 +21,7 @@ const RightPane: React.FC<RightPaneProps> = ({
   return (
     <>
       {/* Code Editor */}
+<<<<<<< HEAD
       <div 
         className="flex-1 min-h-0"
         style={{ 
@@ -28,8 +29,17 @@ const RightPane: React.FC<RightPaneProps> = ({
             ? `calc(100% - ${Math.min(testResultsProps.height || 0, 250)}px - 40px)` 
             : `calc(100% - ${testResultsProps.height || 0}px - 40px)`
         }}
+=======
+      <div
+        className="overflow-hidden"
+        style={{ height: `${100 - ((testResultsProps.height ?? 0.5) * 100)}%` }}
+>>>>>>> 58e6109b (feat: allow code editor and test results to be resized to zero, default to 50/50 split)
       >
-        <CodeEditor {...codeEditorProps} />
+        {(() => {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          const { key, ...rest } = codeEditorProps as any;
+          return <CodeEditor key={key} {...rest} />;
+        })()}
       </div>
 
       {/* Vertical Draggable Divider - Show on desktop */}
@@ -41,10 +51,16 @@ const RightPane: React.FC<RightPaneProps> = ({
       )}
 
       {/* Test Results */}
+<<<<<<< HEAD
       <TestResultsPanel 
         {...testResultsProps} 
         height={isMobile ? Math.min(testResultsProps.height || 0, 250) : testResultsProps.height || 0}
       />
+=======
+      <div style={{ height: `${(testResultsProps.height ?? 0.5) * 100}%` }}>
+        <TestResultsPanel {...testResultsProps} />
+      </div>
+>>>>>>> 58e6109b (feat: allow code editor and test results to be resized to zero, default to 50/50 split)
     </>
   );
 };
